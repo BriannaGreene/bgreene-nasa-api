5bc66d643fea0d94e483d4ec71da4e380560097d
Brianna Greene <brianna.greene.designs@gmail.com>
2017-12-04 22:11:44 -0700

    updated json data

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-let data = require('./javascripts/data')
+let data = require('./javascripts/data.json')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
